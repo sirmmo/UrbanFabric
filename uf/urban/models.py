792f6087716c683c3d7ed4396f52f5fcaabeae15
Marco Montanari <sirmmo@gmail.com>
2010-08-14 19:02:31 +0200

    style, models, index, stuff...

--- uf/urban/models.py
+++ uf/urban/models.py
@@ -5,8 +5,8 @@ from django.contrib.gis.gdal import *
 from django.contrib.gis.geos import *
 
 from django.template.defaultfilters import slugify
-#geographic
 
+#geographic
 class GeoResource(models.Model):
     wkt = models.TextField(blank=True, null=True)
     
@@ -22,7 +22,6 @@ class GeoResource(models.Model):
         gg.object_id = oid
         gg.geolocation = gel
         gg.save()
-
 
 class PointResource(GeoResource):
     geolocation = gismodels.PointField(srid=4326)
@@ -52,7 +51,6 @@ class PolygonResource(GeoResource):
         super(PolygonResource, self).save() # Call the "real" save() method
 
 #interest area
-
 class InterestArea(models.Model):
     wkt = models.TextField(blank = True, null=True)
     geolocation = gismodels.PointField(srid=4326, blank = True, null=True)
@@ -65,6 +63,7 @@ class InterestArea(models.Model):
 
     def __unicode__(self):
         return self.element.name + ": radius = " + str(self.radius)
+
 #classifications
 class Classification(models.Model):
     icon = models.ImageField(upload_to = "img/class/")
@@ -83,6 +82,7 @@ class Classification(models.Model):
         
     def __unicode__(self):
         return self.name
+
 #production
 class Production(models.Model):
     icon = models.ImageField(upload_to = "img/prod/")
@@ -95,8 +95,8 @@ class Production(models.Model):
     def save(self):
         self.slug = slugify(self.name)
         super( Production, self ).save()
-#elements
 
+#elements
 class Venue(PointResource):
     name = models.CharField(max_length=400)
     slug = models.SlugField(max_length=400, unique=True)
@@ -104,7 +104,9 @@ class Venue(PointResource):
     manager = models.ForeignKey(User, related_name="manages_venues")
     classification = models.ManyToManyField(Classification, related_name="classifies")
     products = models.ManyToManyField(Production, related_name="sold")
-    
+
+    opening = models.ManyToManyField('Interval')
+
     def __unicode__(self):
         return self.name
     def save(self):
@@ -119,8 +121,7 @@ class CollectionClassification(models.Model):
     
     def __unicode__(self):
         return self.name
-     def save(self):
-
+    def save(self):
         self.slug = slugify(self.name)
         super(CollectionClassification, self).save() # Call the "real" save() method
         
@@ -132,6 +133,7 @@ class ElementCollection(models.Model):
     classification = models.ManyToManyField(CollectionClassification)
     color_code_back = models.CharField(max_length = 10, default="ff8c00")
     color_code_front = models.CharField(max_length = 10, default="000000")
+    url = models.URLField()
     def __unicode__(self):
         return self.name
 
@@ -140,41 +142,62 @@ class ElementCollection(models.Model):
         self.slug = slugify(self.name)
         super(ElementCollection, self).save() # Call the "real" save() method
 
-    
-
-    
-
-class GeoElementCollection(PolygonResource, ElementCollection):
-    wiki = models.SlugField(max_length = 250)
-    objects = gismodels.GeoManager()
-
-    def save(self):
-
-        super(GeoElementCollection, self).save() # Call the "real" save() method
-
 
 #horrible... to make better asap
 class Message(models.Model):
+    area = models.ForeignKey(InterestArea)
+    timing = models.ManyToManyField('Interval')
+    text = models.CharField(max_length=400)
+
+
+
+DOW = (
+    (0,_('Monday')),
+    (1,_('Tuesday')),
+    (2,_('Wednesday')),
+    (3,_('Thursday')),
+    (4,_('Friday')),
+    (5,_('Saturday')),
+    (6,_('Sunday')),
+)
+
+MOY = (
+    (1,_('January')),
+    (2,_('February')),
+    (3,_('March')),
+    (4,_('April')),
+    (5,_('May')),
+    (6,_('June')),
+    (7,_('July')),
+    (8,_('August')),
+    (9,_('September')),
+    (10,_('October')),
+    (11,_('November')),
+    (12,_('December')),
+)
+
+class Interval(models.Model):
+    repeats = models.BooleanField(default= True)
+
     daily = models.BooleanField(default=False)
     weekly = models.BooleanField(default=False)
     monthly = models.BooleanField(default=False)
     yearly = models.BooleanField(default=False)
 
-    time_start = models.PositiveSmallIntegerField()
-    time_end = models.PositiveSmallIntegerField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
 
-    day_start = models.PositiveSmallIntegerField()
-    day_end = models.PositiveSmallIntegerField()
+    day_start = models.PositiveSmallIntegerField(choices = DOW)
+    day_end = models.PositiveSmallIntegerField(choices = DOW)
 
-    month_start = models.PositiveSmallIntegerField()
-    month_end = models.PositiveSmallIntegerField()
+    month_start = models.PositiveSmallIntegerField(choices = MOY)
+    month_end = models.PositiveSmallIntegerField(choices = MOY)
 
     year_start = models.PositiveIntegerField()
     year_end = models.PositiveIntegerField()
 
-    text = models.CharField(max_length=400)
-
+    
 
 class TrustedServer(models.Model):
-    ping_url = models.UrlField(max_length = 1024, verify_exists = True);
+    ping_url = models.URLField(max_length = 1024, verify_exists = True);
     accepted = models.BooleanField(default = False)
