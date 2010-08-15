a987cdf7ad9575c98749e53af5e338f38177087a
Marco Montanari <sirmmo@gmail.com>
2010-08-15 21:38:46 +0200

    filter arriving

--- uf/urban/views/__init__.py
+++ uf/urban/views/__init__.py
@@ -1,3 +1,4 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.utils import simplejson
@@ -10,3 +11,26 @@ from django.template import RequestContext
 
 def index(request):
     return render_to_response('index.html',{})
+
+def filter(request, filter, mode):
+        parts = filter.split('/')
+        itm = []
+        for i in range(0, len(parts-1)):
+            itm.append(parts[i].split('+'))
+        if len(itm) == 1 :
+            
+        if len(parts) > 1:
+            c = parts[0].split('+')
+            def get_c(el):
+                return Classification.objects.get(el)
+            c = map (get_c,c)
+            p = parts[1].split('+')
+            def get_p(el):
+                return Production.objects.get(el)
+            p = map (get_p,p)
+            pass
+        else: #singlefilter.. normal stuff... but what stuff???
+            if mode.startswith('c'):
+                pass
+            else:
+                pass
